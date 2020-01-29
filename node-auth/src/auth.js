48e31d79b36ed16c06553d3adac6a3528041a34d
Dave12311 <horvath.david987@gmail.com>
2020-01-29 14:29:02 +0100

    Finished authentication, SU and ACL checking

--- node-auth/src/auth.js
+++ node-auth/src/auth.js
@@ -17,18 +17,35 @@ var pool = new pg.Pool({
     idleTimeoutMillis: 30000
 });
 
-// HTTP
-app.use('/http', bodyParser.json());
+app.use(bodyParser.json());
 
-app.use('/http/user', auth_user_http);
-app.use('/http/acl', auth_topic);
-app.use('/http/su', auth_su_http);
+var usernameParser = (req, res, next) => {
+    var decoded = jwt.decode(req.body.username);
+    req.mqtt = {};
+    if(decoded) {
+        req.mqtt.username = decoded.sub;
+        req.mqtt.jwt = true;
+    } else {
+        req.mqtt.username = req.body.username;
+        req.mqtt.jwt = false;
+    }
 
-// JWT
-app.use('/jwt/user', auth_user_jwt);
-app.use('/jwt/acl', bodyParser.json());
-app.use('/jwt/acl', auth_topic);
-//app.use('/jwt/su', debug);
+    next();
+}
+
+app.use(usernameParser);
+
+app.use('/user', auth_user);
+app.use('/acl', auth_topic);
+app.use('/su', auth_su);
+
+function auth_user(req, res) {
+    if(req.mqtt.jwt === true) {
+        auth_user_jwt(req, res);
+    } else {
+        auth_user_http(req, res);
+    }
+}
 
 function auth_user_http(req, res) {
     const query = 'SELECT password FROM mqtt_auth.http_auth WHERE username = $1';
@@ -40,7 +57,7 @@ function auth_user_http(req, res) {
             .compare(req.body.password, result.rows[0].password)
             .then(comp => {
                 if(comp === true) {
-                    console.log('User ' + req.body.username + ' logged in');
+                    console.log('User ' + req.body.username + ' connected using HTTP');
                     res.sendStatus(200);
                 } else {
                     console.log('User ' + req.body.username + ' tried to connect with an incorrect password');
@@ -52,7 +69,7 @@ function auth_user_http(req, res) {
                 res.sendStatus(500);
             });
         } else {
-            console.log('Unregistered user ' + req.body.username + ' tried to log in');
+            console.log('Unregistered user ' + req.body.username + ' tried to connect');
             res.sendStatus(401);
         }
     })
@@ -63,73 +80,49 @@ function auth_user_http(req, res) {
 }
 
 function auth_user_jwt(req, res) {
-    var decoded_jwt = jwt.decode(req.headers.authorization);
-    if(decoded_jwt != null) {
-        const query = 'SELECT pubkey FROM mqtt_auth.jwt_auth WHERE username = $1';
-        pool
-        .query(query, [decoded_jwt.sub])
-        .then(result => {
-            if(result.rowCount === 1) {
-                jwt.verify(req.headers.authorization, result.rows[0].pubkey, function(err, decoded) {
-                    console.log(decoded);
-                    if(err) {
-                        console.log('Invalid JWT for ' + decoded_jwt.sub);
-                        res.sendStatus(401);
-                    } else {
-                        console.log(decoded.sub + ' connected using JWT');
-                        res.sendStatus(200);
-                    }
-                });
-            } else {
-                res.sendStatus(401);
-            }
-        }).catch(err => {
-            console.log(err.stack);
-            res.sendStatus(500);
-        });
-    } else {
-        console.log('Incorrect JWT token');
-        res.sendStatus(400);
-    }
+    const query = 'SELECT pubkey FROM mqtt_auth.jwt_auth WHERE username = $1';
+    pool
+   .query(query, [req.mqtt.username])
+    .then(result => {
+        if(result.rowCount === 1) {
+            jwt.verify(req.body.username, result.rows[0].pubkey, function(err, verified) {
+                if(err) {
+                    console.log('User ' + verified.sub + ' tried to connect using an invalid JWT');
+                    res.sendStatus(401);
+                } else {
+                    console.log('User ' + verified.sub + ' connected using JWT');
+                    res.sendStatus(200);
+                }
+            });
+        } else {
+            console.log('Unregistered user ' + req.mqtt.username + ' tried to connect using JWT');
+            res.sendStatus(401);
+        }
+    }).catch(err => {
+        console.log(err.stack);
+        res.sendStatus(500);
+    });
 }
 
 function auth_topic(req, res) {
-    const query = 'SELECT EXISTS(SELECT 1 FROM mqtt_auth.acl WHERE topic = $1)';
-    console.log(req.body);
-    console.log(req.body.username);
-    console.log(req.headers.authorization);
-    var uname;
-    var decoded = jwt.decode(req.headers.authorization);
-    if(decoded) {
-        uname = decoded.sub;
-    } else {
-        uname = req.body.username;
-    }
-    console.log('UNAME');
-    console.log(uname);
+    const query = 'SELECT CASE WHEN username = $1 THEN acc END FROM mqtt_auth.acl WHERE topic = $2';
     pool
-    .query(query, [req.body.topic])
+    .query(query, [req.mqtt.username, req.body.topic])
     .then(result => {
-        if(result.rows[0].exists === true) {
-            const subquery = 'SELECT acc FROM mqtt_auth.acl WHERE topic = $1 AND username = $2';
-            return pool
-            .query(subquery, [req.body.topic, uname])
-            .then(subresult => {
-                if((subresult.rows[0].acc & req.body.acc) != 0) {
-                    console.log('User ' + uname + ' used protected topic ' + req.body.topic + ' (ACC ' + subresult.rows[0].acc.toString(2) + ') with ACC ' + req.body.acc.toString(2));
-                    res.sendStatus(200);
-                } else {
-                    console.log('User ' + uname + ' tried to use protected topic ' + req.body.topic + ' (ACC ' + subresult.rows[0].acc.toString(2) + ') with ACC ' + req.body.acc.toString(2));
-                    res.sendStatus(401);
-                }
-            })
-            .catch(suberr => {
-                console.log(suberr.stack);
-                res.sendStatus(500);
-            });
-        } else {
-            console.log('User ' + uname + ' used unprotected topic ' + req.body.topic);
+        if(result.rowCount === 0) {
+            console.log('User ' + req.mqtt.username + ' used unprotected topic ' + req.body.topic);
             res.sendStatus(200);
+        } else {
+            if(result.rows[0].case == null) {
+                console.log('User ' + req.mqtt.username + ' tried to use protected topic ' + req.body.topic);
+                res.sendStatus(401);
+            } else if((result.rows[0].case & req.body.acc) != 0) {
+                console.log('User ' + req.mqtt.username + ' used protected topic ' + req.body.topic + ' (ACC ' + result.rows[0].case.toString(2) + ') with ACC ' + req.body.acc.toString(2));
+                res.sendStatus(200);
+            } else {
+                console.log('User ' + req.mqtt.username + ' tried to use protected topic ' + req.body.topic + ' (ACC ' + result.rows[0].case.toString(2) + ') with ACC ' + req.body.acc.toString(2));
+                res.sendStatus(401);
+            }
         }
     })
     .catch(err => {
@@ -138,13 +131,13 @@ function auth_topic(req, res) {
     });
 }
 
-function auth_su_http(req, res) {
+function auth_su(req, res) {
     const query = 'SELECT EXISTS(SELECT 1 FROM mqtt_auth.su WHERE username = $1)';
     pool
-    .query(query, [req.body.username])
+    .query(query, [req.mqtt.username])
     .then(result => {
         if(result.rows[0].exists === true) {
-            console.log('User ' + req.body.username + ' promoted to SU');
+            console.log('User ' + req.mqtt.username + ' used a topic as SU');
             res.sendStatus(200);
         } else {
             res.sendStatus(401);
@@ -154,10 +147,6 @@ function auth_su_http(req, res) {
         console.log(err.stack);
         res.sendStatus(500);
     });
-}
-
-function auth_su_jwt(req, res) {
-
 }
 
 app.listen(3000);
